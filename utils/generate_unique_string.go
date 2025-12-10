73ad8d97c9c4dc4558196c6065d17ff0fa5ec26d
MertJSX <mertJSX@proton.me>
2025-12-10 22:26:16 +0200

    feat: auto secret_jwt_key generation

--- utils/generate_unique_string.go
+++ utils/generate_unique_string.go
@@ -1,6 +1,9 @@
 package utils
 
 import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
 	"strings"
 
 	"github.com/google/uuid"
@@ -10,4 +13,12 @@ func GenerateUniqueString() string {
 	uuid := uuid.New().String()
 	cleanUUID := strings.ReplaceAll(uuid, "-", "")
 	return cleanUUID[:10]
+}
+
+func GenerateJWTKey() (string, error) {
+	bytes := make([]byte, 64) // 512 bit
+	if _, err := rand.Read(bytes); err != nil {
+		return "", fmt.Errorf("random read failed: %w", err)
+	}
+	return hex.EncodeToString(bytes), nil
 }
