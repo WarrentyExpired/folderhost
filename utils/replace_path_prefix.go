87e6dac4c3ad799deb58e73b7170a9de52972aef
MertJSX <mertJSX@proton.me>
2025-12-15 18:53:34 +0200

    fix: ReplacePathPrefix function

--- utils/replace_path_prefix.go
+++ utils/replace_path_prefix.go
@@ -1,12 +1,25 @@
 package utils
 
-import "strings"
+import (
+	"strings"
+)
 
 func ReplacePathPrefix(fullPath string, realPrefix string) string {
 	fullPath = strings.ReplaceAll(fullPath, "\\", "/")
+	realPrefix = strings.ReplaceAll(realPrefix, "\\", "/")
 
 	if strings.HasPrefix(fullPath, realPrefix) {
-		return "./" + fullPath[len(realPrefix):]
+		realPrefixLen := len(realPrefix)
+
+		if realPrefixLen >= len(fullPath) {
+			return "./"
+		}
+
+		if string(fullPath[realPrefixLen]) == "/" {
+			return "." + fullPath[realPrefixLen:]
+		}
+
+		return "./" + fullPath[realPrefixLen:]
 	}
 
 	return fullPath
